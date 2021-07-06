ca2950b961b4f1a9832a8e19ab2456804dac2025
Sergei_L <lubeshkosergei@gmail.com>
2021-07-07 00:27:57 +0300

    change url, create def, add templates

--- vacancy/urls.py
+++ vacancy/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 
-from vacancy.views import main_view
+from vacancy.views import main_view, show_list_all_vacancies, show_list_specialty_vacancies
 
 urlpatterns = [
     path('', main_view, name='main'),
+    path('vacancies/cat/<str:vacancy>/', show_list_specialty_vacancies, name='specialty_vacancies'),
+    path('vacancies/', show_list_all_vacancies, name='list_vacancies'),
+
 ]
--- vacancy/views.py
+++ vacancy/views.py
@@ -3,3 +3,11 @@ from django.shortcuts import render
 
 def main_view(request):
     return render(request, template_name='vacancy/index.html')
+
+
+def show_list_all_vacancies(request):
+    return render(request, template_name='vacancy/vacancy-list.html')
+
+
+def show_list_specialty_vacancies(request, vacancy):
+    return render(request, template_name='vacancy/vacancy.html')
